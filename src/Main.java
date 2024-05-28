7b9e7d0be8a65366e204bc987660cd23db6e6b71
Admin <v.i.p@bk.ru>
2024-05-28 15:08:05 +0300

    fixed it

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,22 @@ public class Main {
         double total = 0;
         int deposite = 15000;
         double percentPerMonth = 1.01;
-        int Month = 1;
+        int month = 1;
         while (total < 2_459_000) {
             total = (total + deposite) * percentPerMonth;
-            System.out.printf("«Месяц %s,сумма накоплений равна %.2f рублей»%n", Month, total);
-            Month++;
+            System.out.printf("«Месяц %s,сумма накоплений равна %.2f рублей»%n", month, total);
+            month++;
         }
         System.out.println();
         System.out.println("Задача-2");
-        int conter = 0;
-        while (conter < 10) {
-            conter++;
-            System.out.print(conter + " ");
+        int counter = 0;
+        while (counter < 10) {
+            counter++;
+            System.out.print(counter + " ");
         }
         System.out.println();
-        for (; conter >= 1; conter--) {
-            System.out.print(conter + " ");
+        for (; counter >= 1; counter--) {
+            System.out.print(counter + " ");
         }
         System.out.println();
         System.out.println("Задача-3");
@@ -32,49 +32,45 @@ public class Main {
             birthRate = population / 1000 * 17;
             mortalityRate = population / 1000 * 8;
             population += birthRate - mortalityRate;
-            System.out.println(" « Год " +year+ " численность населения составляет " + population+ "»");
+            System.out.println(" « Год " + year + " численность населения составляет " + population + "»");
         }
         System.out.println();
-        System.out.println("Задача-4");
+        System.out.println("Задача-4-5");
         double vasyPercent = 1.07;
         double vasyDeposite = 15000;
         int vasyMonth = 1;
         while (vasyDeposite < 12_000_000) {
             vasyDeposite *= vasyPercent;
-            System.out.printf("Месяц %s депозит составит: %.2f%n", vasyMonth, vasyDeposite);
+            if (vasyMonth % 6 == 0) {
+                System.out.printf("Month %s : deposite: %.2f%n", vasyMonth, vasyDeposite);
+            }
             vasyMonth++;
-
         }
-        System.out.println();
-        System.out.println("Задача-5");
-        double vasyPercent1 = 1.07;
-        double vasyDeposite1 = 15000;
-        int vasyMonth1 = 1;
-        while (vasyDeposite1 < 12_000_000) {
-            vasyDeposite1 *= vasyPercent1;
-            if (vasyMonth1 % 6 == 0)
-                System.out.printf("Месяц %s депозит составит: %.2f%n", vasyMonth1, vasyDeposite1);
-            vasyMonth1++;
 
-        }
         System.out.println();
         System.out.println("Задача-6");
-        vasyPercent1 = 1.07;
-        vasyDeposite1 = 15000;
-        vasyMonth1 = 1;
+        vasyPercent = 1.07;
+        vasyDeposite = 15000;
+        vasyMonth = 1;
         int targetMonthsAmount = 9 * 12;
-        for (int i = 1; i <= targetMonthsAmount; i++) {
-            vasyDeposite1 *= vasyPercent1;
+        for (
+                int i = 1;
+                i <= targetMonthsAmount; i++) {
+            vasyDeposite *= vasyPercent;
             if (i % 6 == 0)
-                System.out.printf("Месяц %s депозит составит: %.2f%n", i, vasyDeposite1);
+                System.out.printf("Месяц %s депозит составит: %.2f%n", i, vasyDeposite);
 
         }
         System.out.println();
         System.out.println("Задача-7");
-        int friday = 3;
-        for (int i = 1; i < 31; i++) {
+        int friday = 2;
+        for (
+                int i = 1;
+                i < 31; i++) {
             if (i == friday || (i - friday) % 7 == 0) {
-                System.out.printf("«Сегодня пятница,%s-eчисло. Необходимо подготовить отчет»%n", i);
+                System.out.printf("«Сегодня пятница,%s-eчисло. Необходимо подготовить отчет»%n", i);// В результате у вас должно получиться от 4 до 5 сообщений с напоминаниями по разным датам.
+
+
             }
         }
         System.out.println();
@@ -83,14 +79,39 @@ public class Main {
         int curentYear = 2024;
         int lowerBound = curentYear - 200;
         int uperBound = curentYear + 100;
-        for (int i = 0; i < uperBound; i += period) {
+        for (
+                int i = 0;
+                i < uperBound; i += period) {
             if (i > lowerBound) {
-                System.out.println(i);
+                System.out.println(i);// проверил все выводится корректно.
+
             }
-
         }
-
-
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
